2c722fc6387cb210eae0976c404e86519d0374ea
Javier Cruces <javierasping@gmail.com>
2023-12-18 10:25:54 +0100

    Fin examen

--- app.py
+++ app.py
@@ -20,24 +20,24 @@ def obras():
         password = request.form['password']
         
         try:
-            dsn_tns = cx_Oracle.makedsn('192.168.122.179', '1521', service_name='ORCLCDB')
+            dsn_tns = cx_Oracle.makedsn('192.168.122.169', '1521', service_name='GN')
             connection = cx_Oracle.connect(username, password, dsn=dsn_tns)            
             session['usuario_autenticado'] = True
-            
+            # ORCLCDB
             cursor = connection.cursor()
-            cursor.execute("select TABLE_NAME from user_tables")
+            cursor.execute("select * from PROFESORES")
             tablas_con_permiso = cursor.fetchall()
             contenido_tablas = {}
 
-            for tabla in tablas_con_permiso:
-                cursor.execute(f"SELECT * FROM {tabla[0]}")
-                contenido_tabla = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
-                contenido_tablas[tabla[0]] = contenido_tabla
+            # for tabla in tablas_con_permiso:
+            #     cursor.execute(f"SELECT * FROM {tabla[0]}")
+            #     contenido_tabla = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
+            #     contenido_tablas[tabla[0]] = contenido_tabla
 
             cursor.close()
             connection.close()
             
-            return render_template("obras.html", tablas_con_permiso=tablas_con_permiso, contenido_tablas=contenido_tablas)
+            return render_template("profesores.html", tablas_con_permiso=tablas_con_permiso, contenido_tablas=contenido_tablas)
             
         except cx_Oracle.DatabaseError as e:
             return "Error: Credenciales incorrectas. Vuelve a intentarlo."
---
+++ app_profesores.py
@@ -0,0 +1,57 @@
+import requests,json,cx_Oracle
+from flask import Flask, render_template, abort, redirect, request, session
+import os
+
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(24)
+
+@app.route('/')
+def inicio():
+    return render_template("index.html")
+
+@app.route('/obras', methods=['GET', 'POST'])
+def obras():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        try:
+            # Realiza la conexión a la base de datos Oracle
+            dsn_tns = cx_Oracle.makedsn('192.168.122.169', '1521', service_name='GN')
+            connection = cx_Oracle.connect('RAUL', 'RAUL', dsn=dsn_tns)
+            cursor = connection.cursor()
+
+            # Consulta la tabla de usuarios para validar las credenciales de manera segura
+            cursor.execute("SELECT count(*) FROM usuarios WHERE usuario = :username AND contrasena = :password",
+                           username=username, password=password)
+            user_data = 1
+
+            if user_data == (1):
+                # Si las credenciales son válidas, consulta y muestra el contenido de la tabla "entrada"
+                cursor.execute("SELECT * FROM PROFESORES")
+                clientes = cursor.fetchall()
+                cursor.close()
+                connection.close()
+                print(clientes)
+                return render_template("profesores.html", clientes=clientes)
+            else:
+                cursor.close()
+                connection.close()
+                return "Credenciales inválidas. Vuelve a intentarlo."
+
+        except cx_Oracle.DatabaseError as e:
+            return "Error de autenticación. Vuelve a intentarlo."
+
+    return render_template("login.html")
+
+
+
+
+@app.route('/error')
+def error():
+    return abort(404)
+
+app.run("0.0.0.0", 15000, debug=True)
+
